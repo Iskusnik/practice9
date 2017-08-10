9687f8e4e44725dd8e6527d7264522c12d5ead46
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-08-10 15:45:25 +0500

    Реализован Лист (список)

---
+++ practice9/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice9
+{
+    class Point
+    {
+        int Info { get; set; }
+        Point Next { get; set; }
+        public Point(int info)
+        {
+            Info = info;
+            Next = null;
+        }
+        public static implicit operator bool(Point point)
+        {
+            if (point.Info >= 0)
+                return true;
+            else
+                return false;
+        }
+    }
+}
--- practice9/Program.cs
+++ practice9/Program.cs
@@ -8,8 +8,27 @@ namespace practice9
 {
     class Program
     {
+     /// <summary>
+     /// 27.	
+     /// В программе построен линейный список. 
+     /// Напишите процедуру подсчета двух сумм: всех положительных и всех отрицательных значений, записанных в информационные поля элементов списка.
+     /// </summary>
+     /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Point test;
+            test = new Point(-1);
+            Console.WriteLine(test);         //False
+            Console.WriteLine(test == false);//false == false Ответ: True
+
+            test = new Point(1);
+            Console.WriteLine(test);         //True
+            Console.WriteLine(test == false);//true == false Ответ: False
+
+            test = new Point(0);
+            Console.WriteLine(test);         //True
+            Console.WriteLine(test == false);//true == false Ответ: False
+
         }
     }
 }
